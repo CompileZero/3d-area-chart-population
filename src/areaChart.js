5fa90445a22f78e9bb282e3b83164e85bad9330a
atharva <atharva.kulkarni@helpshift.com>
2023-01-04 17:28:37 +0530

    basic area chart

--- src/areaChart.js
+++ src/areaChart.js
@@ -2,23 +2,24 @@ import * as d3 from "d3";
 import { randomExponential } from "d3";
 
 export function renderAreaChart(data) {
+  const allYearsData = data.Values;
+
   let svg = d3
     .select(".container")
     .append("svg")
     .attr("width", 500)
-    .attr("height", 1500);
+    .attr("height", 500);
 
-  const tParser = d3.timeParse("%Y");
-  const allYearsData = data.Values;
+  const timeParser = d3.timeParse("%Y");
 
   let x = d3
     .scaleTime()
-    .domain(d3.extent(allYearsData, (d) => tParser(d.Year)))
+    .domain(d3.extent(allYearsData, (d) => timeParser(d.Year)))
     .range([0, 500]);
 
   console.log(d3.extent(allYearsData, (d) => d.Count));
 
-  let y = d3.scalePow().exponent(0.2).domain([0, 1400000000]).range([1500, 0]);
+  let y = d3.scaleLinear().domain([0, 1000000000]).range([500, 0]);
 
   svg
     .append("path")
@@ -28,9 +29,8 @@ export function renderAreaChart(data) {
       "d",
       d3
         .area()
-        .x((d) => x(tParser(d.Year)))
+        .x((d) => x(timeParser(d.Year)))
         .y0(y(0))
         .y1((d) => y(d.Count))
-        .curve(d3.curveCardinalOpen)
     );
 }
